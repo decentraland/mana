5d6a5c5a0a9fc7f57496f25899231ddad0dc7d75
Federico Bond <federicobond@gmail.com>
2017-08-01 04:19:29 -0300

    Implement PausableToken for MANA. Closes #22

--- contracts/MANAToken.sol
+++ contracts/MANAToken.sol
@@ -1,13 +1,18 @@
 pragma solidity ^0.4.11;
 
+import "zeppelin-solidity/contracts/token/PausableToken.sol";
 import "zeppelin-solidity/contracts/token/MintableToken.sol";
 import "./BurnableToken.sol";
 
-contract MANAToken is BurnableToken, MintableToken {
+contract MANAToken is BurnableToken, PausableToken, MintableToken {
 
     string public constant symbol = "MANA";
 
     string public constant name = "Decentraland MANA";
 
     uint8 public constant decimals = 18;
+
+    function burn(uint256 _value) whenNotPaused public {
+        super.burn(_value);
+    }
 }
