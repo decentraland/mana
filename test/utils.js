c191e03f825f10a0a2b507a1a6465efb83b0454c
Ariel Barmat <abarmat@decentraland.org>
2018-08-03 17:19:22 -0300

    fix: solve tests with new version of libs

--- test/utils.js
+++ test/utils.js
@@ -12,11 +12,15 @@ type RPCParams = {
   params?: Array<mixed>
 }
 
-function rpcCall (call: RPCParams) {
-  const payload: any = Object.assign({}, {
-    jsonrpc: '2.0',
-    id: Date.now()
-  }, call)
+function rpcCall(call: RPCParams) {
+  const payload: any = Object.assign(
+    {},
+    {
+      jsonrpc: '2.0',
+      id: Date.now()
+    },
+    call
+  )
 
   return new Promise((resolve, reject) => {
     web3.currentProvider.sendAsync(payload, (err, res) => {
@@ -25,18 +29,22 @@ function rpcCall (call: RPCParams) {
   })
 }
 
-export function advanceBlock () {
+export function advanceBlock() {
   return rpcCall({ method: 'evm_mine' })
 }
 
-export function increaseTime (seconds: number) {
+export function increaseTime(seconds: number) {
   return rpcCall({ method: 'evm_increaseTime', params: [+seconds] })
 }
 
 // Advances the block number so that the last mined block is `number`.
-export async function advanceToBlock (number: number) {
+export async function advanceToBlock(number: number) {
   if (web3.eth.blockNumber > number) {
-    throw Error(`block number ${number} is in the past (current is ${web3.eth.blockNumber})`)
+    throw Error(
+      `block number ${number} is in the past (current is ${
+        web3.eth.blockNumber
+      })`
+    )
   }
 
   while (web3.eth.blockNumber < number) {
@@ -44,22 +52,25 @@ export async function advanceToBlock (number: number) {
   }
 }
 
-export async function advanceToTime (timestamp: number) {
+export async function advanceToTime(timestamp: number) {
   const block = await web3.eth.getBlock('latest')
   if (block.timestamp > timestamp) {
-    throw Error(`time ${timestamp} is in the past (current is ${block.timestamp})`)
+    throw Error(
+      `time ${timestamp} is in the past (current is ${block.timestamp})`
+    )
   }
 
   await advanceTime(timestamp - block.timestamp)
 }
 
-export async function advanceTime (time: number) {
+export async function advanceTime(time: number) {
   await increaseTime(time)
   await advanceBlock()
 }
 
-export function ether (n: number) {
+export function ether(n: number) {
   return new web3.BigNumber(web3.toWei(n, 'ether'))
 }
 
 export const EVMThrow = 'invalid opcode'
+export const EVMRevert = 'revert'
